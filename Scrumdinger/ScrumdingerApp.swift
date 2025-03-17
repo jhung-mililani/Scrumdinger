993ce8fe3afdd576598f11d0cf0bdaf3a948960b
jhung-mililani <115854825+jhung-mililani@users.noreply.github.com>
2025-03-17 13:19:52 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by jhung on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
